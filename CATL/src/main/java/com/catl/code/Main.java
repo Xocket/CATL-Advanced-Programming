6be59c71cb1d7ba90aafd49c5be38c19b45d1a3f
Xocket <62717100+Xocket@users.noreply.github.com>
2024-04-30 18:02:04 +0200

    Modified .gitignore and created Program class which will contain the Program's logic.

--- CATL/src/main/java/com/catl/code/Main.java
+++ CATL/src/main/java/com/catl/code/Main.java
@@ -1,8 +1,12 @@
+// Package declaration.
 package com.catl.code;
 
+// The Main class serves as the entry point of the program.
 public class Main {
 
+    // The main method execution.
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Running the Program code.
+        new Program().run();
     }
 }
---
+++ CATL/src/main/java/com/catl/code/Program.java
@@ -0,0 +1,11 @@
+// Package declaration.
+package com.catl.code;
+
+// The Program class contains the execution code for the program.
+public class Program {
+
+    // The run method contains the Program logic.
+    public void run() {
+        // WIP - Program code.
+    }
+}
