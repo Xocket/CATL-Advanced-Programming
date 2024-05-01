364e42c7aca0801b3bd30fd190946100aaa661c4
Xocket <62717100+Xocket@users.noreply.github.com>
2024-05-01 14:43:02 +0200

    Created Log class to store Airport events, modified .gitignore to ignore the airportEvolution.txt file.

---
+++ CATL/src/main/java/com/catl/code/Log.java
@@ -0,0 +1,41 @@
+package com.catl.code;
+
+import java.io.*;
+import java.util.concurrent.locks.*;
+
+public class Log {
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private final String logFolderName = "Log";
+    private final String logFileName = "airportEvolution.txt";
+
+    public void logEvent(String airport, String event) {
+        lock.lock();
+        try {
+            // Get the current working directory
+            String currentDir = System.getProperty("user.dir");
+            // Create the relative file path for the Log folder
+            File logDir = new File(currentDir + File.separator + logFolderName);
+            File logFile = new File(logDir, logFileName);
+
+            // Check if the Log directory exists, if not, create it
+            if (!logDir.exists()) {
+                logDir.mkdirs();
+            }
+
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(logFile, false)));
+
+            String timeStamp = new java.text.SimpleDateFormat("yyyy/MM/dd - HH:mm:ss:SSS").format(new java.util.Date());
+
+            out.println("------------------------------ AIRPORT EVOLUTION ------------------------------\n");
+            out.println(timeStamp + " - " + airport + " - " + event);
+            out.close();
+
+        } catch (IOException e) {
+            System.err.println("There was a problem writing to the log file");
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
--- CATL/src/main/java/com/catl/code/Program.java
+++ CATL/src/main/java/com/catl/code/Program.java
@@ -7,6 +7,10 @@ public class Program {
     // The run method contains the Program logic.
     public void run() {
 
+        // Create Log object to store events.
+        Log log = new Log();
+        log.logEvent("bruh", "second bruh");
+
         // Create both airports.
         Airport madridAirport = new Airport();
         Airport barcelonaAirport = new Airport();
